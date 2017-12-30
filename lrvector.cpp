4b401bbab082ad883c0e4506850e83af4918a6b3
kyosheek <31596992+kyosheek@users.noreply.github.com>
2017-12-30 18:05:10 +0300

    Update lrvector.cpp

--- lrvector.cpp
+++ lrvector.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-LRMatrix::LRMatrix(vector<vector<double>> x, vector<double> y, int m) {
+LRVector::LRVector(vector<vector<double>> x, vector<double> y, int m) {
 	this->x = x;
 	this->y = y;
 	this->m = m;
 }
 
-void LRMatrix::train(double alpha, int iter) {
+void LRVector::train(double alpha, int iter) {
 	auto *J = new double[iter];
 	this->t = graident_descent(x, y, alpha, J, iter, m);
 
@@ -25,11 +25,11 @@ void LRMatrix::train(double alpha, int iter) {
 	}
 }
 
-double LRMatrix::predict(vector<double> x) {
+double LRVector::predict(vector<double> x) {
 	return h(x, t);
 }
 
-double LRMatrix::calc_cost(vector<vector<double>> x, vector<double> y, vector<double> t, int m) {
+double LRVector::calc_cost(vector<vector<double>> x, vector<double> y, vector<double> t, int m) {
 	vector<double> preds = calc_pred(x, t, m);
 
 	vector<double> diff;
@@ -44,7 +44,7 @@ double LRMatrix::calc_cost(vector<vector<double>> x, vector<double> y, vector<do
 	return ((1.0 / (2 * m)) * s);
 }
 
-double LRMatrix::h(vector<double> x, vector<double> t) {
+double LRVector::h(vector<double> x, vector<double> t) {
 	double hyp = 0;
 	for (int i = 0; i < t.size(); ++i) {
 		if (i == 0) { hyp += t[i]; }
@@ -53,22 +53,22 @@ double LRMatrix::h(vector<double> x, vector<double> t) {
 	return hyp;
 }
 
-vector<double> LRMatrix::calc_pred(vector<vector<double>> x, vector<double> t, int m) {
+vector<double> LRVector::calc_pred(vector<vector<double>> x, vector<double> t, int m) {
 	vector<double> pred;
 
 	//vector size
 	//calculate h for each training data set
-	for (int i = 0; i < m; ++i) { pred.push_back(LRMatrix::h(x[i], t)); }
+	for (int i = 0; i < m; ++i) { pred.push_back(LRVector::h(x[i], t)); }
 
 	return pred;
 }
 
-vector<double> LRMatrix::graident_descent(vector<vector<double>>x, vector<double> y, double alpha, double *J, int iter, int m) {
+vector<double> LRVector::graident_descent(vector<vector<double>>x, vector<double> y, double alpha, double *J, int iter, int m) {
 	vector<double> t;
 	for (int i = 0; i <= x[0].size(); i++) { t.push_back(1); }
 
 	for (int i = 0; i < iter; ++i) {
-		vector<double> pred = LRMatrix::calc_pred(x, t, m);
+		vector<double> pred = LRVector::calc_pred(x, t, m);
 		vector<double> diff;
 		for (int j = 0; j < m; ++j) { diff.push_back(pred[j] - y[j]); }
 		
@@ -93,7 +93,7 @@ vector<double> LRMatrix::graident_descent(vector<vector<double>>x, vector<double
 			sum = 0;
 		}
 
-		J[i] = LRMatrix::calc_cost(x, y, t, m);
+		J[i] = LRVector::calc_cost(x, y, t, m);
 	}
 
 	return t;
